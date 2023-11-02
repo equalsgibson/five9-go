0fa85a64309059ecc0746b2abf5a130afd78bcbf
Chris Gibson <cgibson@datto.com>
2023-11-02 12:52:04 +0000

    change terminology

--- ops/domainCheck.sh
+++ ops/domainCheck.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ## Following domains are allowed to be included in files.
-WHITELISTEDDOMAINS=("example.com" "api.prod.us.five9.net" "app.five9.com" "cobertura.sourceforge.net")
+ALLOWLISTEDDOMAINS=("example.com" "api.prod.us.five9.net" "app.five9.com" "cobertura.sourceforge.net")
 
 ## Remove the trailing \| from the joined string
-printf -v joinedWhitelist '%s\|' "${WHITELISTEDDOMAINS[@]}"
-DOMAINSTRING=$(echo "${joinedWhitelist%\\|}")
+printf -v joinedAllowlist '%s\|' "${ALLOWLISTEDDOMAINS[@]}"
+DOMAINSTRING=$(echo "${joinedAllowlist%\\|}")
 
 if [ "$(grep -rI -Eo '(www\.|:\/\/|@)([a-zA-Z][a-zA-Z0-9\-_]{1,61}).?([a-zA-Z][a-zA-Z0-9\-_]{1,61}\.?)+\/?' five9 | grep -vc ${DOMAINSTRING} )" -gt 0 ]; 
 then 
-    echo "Found a non whitelisted domain:";
+    echo "Found a non allowlisted domain:";
     grep -rI -Eo '(www\.|:\/\/|@)([a-zA-Z][a-zA-Z0-9\-_]{1,61}).?([a-zA-Z][a-zA-Z0-9\-_]{1,61}\.?)+\/?' five9 | grep -v ${DOMAINSTRING}
     exit 1; 
 fi
